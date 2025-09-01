53e3e252492ed72242d8688de49b3f7a0337166b
Shalini Singh <163624964+Oblivion634@users.noreply.github.com>
2025-09-01 06:19:44 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -160,7 +160,7 @@ function Contact() {
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -20 }}
-            className={`fixed top-5 left-1/2 transform -translate-x-1/2 px-6 py-3 rounded-lg shadow-lg z-20
+            className={`fixed top-5 left-1/2 transform -translate-x-1/2 px-6 py-3 rounded-lg shadow-lg z-60
                ${
                  popup.type === "success" ? "bg-green-500" : "bg-red-500"
                } text-white`}
